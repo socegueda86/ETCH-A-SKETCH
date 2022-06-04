ae0d3313c9b0856c44652e54afbedb2bb2c39939
socegueda86 <socegueda86@gmail.com>
2022-06-04 02:20:13 -0500

    Add press fx to create a new grid, but the validation function doesnt work yet, be careful it causes infinite loops if while parameters arent the correct ones

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function nValidation (){
+    let n2;
     do{
-    n = prompt("Chose a number up to 100")
+    n2 = prompt("Chose a number up to 100")
     } while (!(n > 0 || n < 100 || n == null || parseInt(n) == NaN)) //
-    return n
+    return n2
 }
-
-
 
 function random255(){
     return Math.floor(Math.random()*256)
@@ -16,14 +15,13 @@ function createGrit(n = 16){
         let item = document.createElement('div');
         item.classList.add('class', `item`);
         container.appendChild(item);
+        
     };
-
     let squares = document.querySelectorAll(".item");
     squares.forEach(square => {
     square.addEventListener('mouseover', function(e){e.target.style.background = `rgb(${random255()},    ${random255()},${random255()})`})
     });
 }
-
 
 function erase() {
     let toErase = document.querySelectorAll('.item');
@@ -32,16 +30,22 @@ function erase() {
     })
 }
 
+function press (){
+    let n2 = nValidation();
+    erase();
+    createGrit(n2);
+}
+
 
 /************************************functions above -************ */
 
 
-let n;
+const n = 16;
+const container = document.querySelector('.grid-container');
 
 const pressMe = document.querySelector('.n-generator');
 pressMe.addEventListener('click', nValidation);
 
-let container = document.querySelector('.grid-container');
 container.setAttribute('style', 'grid-template-columns: auto auto auto auto;');
 createGrit(n);
 
