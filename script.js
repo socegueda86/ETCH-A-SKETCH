47db3c2c927613d1c356a3d2e62bf00e3f3e9da4
socegueda86 <socegueda86@gmail.com>
2022-06-04 02:03:05 -0500

    Add Erase Fx and the PressMe button has JS to ask-for and validate input

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function nValidation (){
+    do{
+    n = prompt("Chose a number up to 100")
+    } while (!(n > 0 || n < 100 || n == null || parseInt(n) == NaN)) //
+    return n
+}
+
+
+
 function random255(){
     return Math.floor(Math.random()*256)
 }
@@ -8,22 +17,34 @@ function createGrit(n = 16){
         item.classList.add('class', `item`);
         container.appendChild(item);
     };
-    
+
     let squares = document.querySelectorAll(".item");
     squares.forEach(square => {
     square.addEventListener('mouseover', function(e){e.target.style.background = `rgb(${random255()},    ${random255()},${random255()})`})
     });
-
-
-
-    //container.addEventListener('mouseover', function(e){e.target.style.background = `rgb(${random255()},    ${random255()},${random255()})`})
 }
+
+
+function erase() {
+    let toErase = document.querySelectorAll('.item');
+    toErase.forEach(element => {
+    container.removeChild(element)
+    })
+}
+
+
+/************************************functions above -************ */
+
+
+let n;
+
+const pressMe = document.querySelector('.n-generator');
+pressMe.addEventListener('click', nValidation);
 
 let container = document.querySelector('.grid-container');
 container.setAttribute('style', 'grid-template-columns: auto auto auto auto;');
-createGrit();
+createGrit(n);
 
-/*let squares = document.querySelectorAll(".item");
-squares.forEach(square => {
-    square.addEventListener('mouseover', function(e){e.target.style.background = `rgb(${random255()},    ${random255()},${random255()})`})
-});*/
+
+
+
